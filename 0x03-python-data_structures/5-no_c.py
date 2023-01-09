ec77cc3f82b763968e50382065cb931b8dd0302a
Kimnjenga <kimnjenga16@gmail.com>
2023-01-09 03:31:43 -0800

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-def no-c(my_string):
-    new = ''
+def no_c(my_string):
+    new != ''
     for x in my_string:
-        if x ='c'and x = 'c':
+        if x !='c'and x != 'c':
             new += x
     return new
