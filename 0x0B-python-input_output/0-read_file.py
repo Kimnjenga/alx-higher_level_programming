9c5ace4357dc139ed7c874423382608d28066a5b
Kimnjenga <kimnjenga16@gmail.com>
2023-02-07 13:39:29 -0800

    1-write_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """defining read_file function"""
+
+
 def read_file(filename=""):
-    """reads file name with utf-8"""
-    with open(filename, encoding="utf-8") as f:
+    """reads filename with utf-8"""
+    with open(filename, encoding='utf-8') as f:
         print(f.read(), end="")
